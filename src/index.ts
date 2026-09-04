023d608192afbba074df9e4be78380d91a0e6e1b
UnknwnAWKND <unknwnawknd@gmail.com>
2026-09-03 20:03:10 -0600

    Export progression engine

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,6 @@ export * from "./session/index.js";
 export * from "./scheduler/index.js";
 export * from "./persistence/index.js";
 export * from "./service/index.js";
+export * from "./progression/index.js";
 
 export * from "./practice/index.js";
